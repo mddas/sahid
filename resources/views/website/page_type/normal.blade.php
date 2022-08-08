151954907cbb78cd057483f19ef15d6b4a708a09
md <mddasgudiya@gmail.com>
2022-08-09 00:00:16 +0545

    full normal page

--- resources/views/website/page_type/normal.blade.php
+++ resources/views/website/page_type/normal.blade.php
@@ -17,19 +17,108 @@
         </div><!-- /.row -->
       </div><!-- /.container -->
     </section>
-
-    <section class="department-page bg-gray">
-	<div class="container">
-		<div class="row">
-			<div class="col-lg-12">
-				<div class="department-detail">
-					<h2 id="introduction">{{$normal->caption}}</h2>
-					<p>@php echo $normal->long_content ; @endphp</p>
-					<img src="{{$normal->banner_image}}">		       
-				</div>
-			</div>
-		</div>
-	</div>
+<!----full page---->
+    <section class="sahid-services">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-4 col-md-6 col-sm-12">
+                <div class="service-item">
+                    <div class="service__icon">
+                        <img src="images/icon-8.png" alt=""/>
+                        <img src="images/icon-8.png" alt=""/>
+                    </div><!-- /.service__icon -->
+                    <div class="service__content">
+                        <h4 class="service__title">Neurology Department</h4>
+                        <p class="service__desc">The Neurology department specialises in the diagnosis and treatment of disorders of the nervous system – including the brain, spinal cord, peripheral nerves and muscles.</p>
+                      <a href="department.php" class="btn btn-outline">
+                          <span>Read More</span>
+                          <i class="fa fa-arrow-right"></i>
+                      </a>
+                    </div><!-- /.service__content -->
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6 col-sm-12">
+                <div class="service-item">
+                    <div class="service__icon">
+                        <img src="images/icon-8.png" alt=""/>
+                        <img src="images/icon-8.png" alt=""/>
+                    </div><!-- /.service__icon -->
+                    <div class="service__content">
+                        <h4 class="service__title">Neurology Department</h4>
+                        <p class="service__desc">The Neurology department specialises in the diagnosis and treatment of disorders of the nervous system – including the brain, spinal cord, peripheral nerves and muscles.</p>
+                      <a href="department.php" class="btn btn-outline">
+                          <span>Read More</span>
+                          <i class="fa fa-arrow-right"></i>
+                      </a>
+                    </div><!-- /.service__content -->
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6 col-sm-12">
+                <div class="service-item">
+                    <div class="service__icon">
+                        <img src="images/icon-8.png" alt=""/>
+                        <img src="images/icon-8.png" alt=""/>
+                    </div><!-- /.service__icon -->
+                    <div class="service__content">
+                        <h4 class="service__title">Neurology Department</h4>
+                        <p class="service__desc">The Neurology department specialises in the diagnosis and treatment of disorders of the nervous system – including the brain, spinal cord, peripheral nerves and muscles.</p>
+                      <a href="department.php" class="btn btn-outline">
+                          <span>Read More</span>
+                          <i class="fa fa-arrow-right"></i>
+                      </a>
+                    </div><!-- /.service__content -->
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6 col-sm-12">
+                <div class="service-item">
+                    <div class="service__icon">
+                        <img src="images/icon-8.png" alt=""/>
+                        <img src="images/icon-8.png" alt=""/>
+                    </div><!-- /.service__icon -->
+                    <div class="service__content">
+                        <h4 class="service__title">Neurology Department</h4>
+                        <p class="service__desc">The Neurology department specialises in the diagnosis and treatment of disorders of the nervous system – including the brain, spinal cord, peripheral nerves and muscles.</p>
+                      <a href="department.php" class="btn btn-outline">
+                          <span>Read More</span>
+                          <i class="fa fa-arrow-right"></i>
+                      </a>
+                    </div><!-- /.service__content -->
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6 col-sm-12">
+                <div class="service-item">
+                    <div class="service__icon">
+                        <img src="images/icon-8.png" alt=""/>
+                        <img src="images/icon-8.png" alt=""/>
+                    </div><!-- /.service__icon -->
+                    <div class="service__content">
+                        <h4 class="service__title">Neurology Department</h4>
+                        <p class="service__desc">The Neurology department specialises in the diagnosis and treatment of disorders of the nervous system – including the brain, spinal cord, peripheral nerves and muscles.</p>
+                      <a href="department.php" class="btn btn-outline">
+                          <span>Read More</span>
+                          <i class="fa fa-arrow-right"></i>
+                      </a>
+                    </div><!-- /.service__content -->
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6 col-sm-12">
+                <div class="service-item">
+                    <div class="service__icon">
+                        <img src="images/icon-8.png" alt=""/>
+                        <img src="images/icon-8.png" alt=""/>
+                    </div><!-- /.service__icon -->
+                    <div class="service__content">
+                        <h4 class="service__title">Neurology Department</h4>
+                        <p class="service__desc">The Neurology department specialises in the diagnosis and treatment of disorders of the nervous system – including the brain, spinal cord, peripheral nerves and muscles.</p>
+                      <a href="department.php" class="btn btn-outline">
+                          <span>Read More</span>
+                          <i class="fa fa-arrow-right"></i>
+                      </a>
+                    </div><!-- /.service__content -->
+                </div>
+            </div>
+        </div>
+    </div>
 </section>
 
 @endsection
