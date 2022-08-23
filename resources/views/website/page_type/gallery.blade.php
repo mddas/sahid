c40a744cfe2b51a8e3d6b9148a40a349922f3b9e
md <mddasgudiya@gmail.com>
2022-08-23 12:23:19 +0545

    slug

--- resources/views/website/page_type/gallery.blade.php
+++ resources/views/website/page_type/gallery.blade.php
@@ -9,8 +9,11 @@
             <h1 class="pagetitle__heading my-3">{{$slug_detail->caption ?? $slug1}}</h1>
             <nav>
               <ol class="breadcrumb my-3">
+				<li class="breadcrumb-item"><a href="/">Home</a></li>
                 <li class="breadcrumb-item"><a href="#">{{$slug1}}</a></li>
-                <li class="breadcrumb-item">{{$slug_detail->nav_name ?? ''}}</li>
+				@if(isset($slug_detail))
+                	<li class="breadcrumb-item">{{$slug_detail->caption ?? ''}}</li>
+				@endphp
               </ol>
             </nav>
           </div><!-- /.col-12 -->
