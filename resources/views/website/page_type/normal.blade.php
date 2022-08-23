3883a488393c8438740b9f16d4fcabca8892e127
md <mddasgudiya@gmail.com>
2022-08-23 12:06:38 +0545

    bitcoin home

--- resources/views/website/page_type/normal.blade.php
+++ resources/views/website/page_type/normal.blade.php
@@ -9,6 +9,7 @@
             <h1 class="pagetitle__heading my-3">{{$slug_detail->caption ?? $slug1}}</h1>
             <nav>
               <ol class="breadcrumb my-3">
+                <li class="breadcrumb-item"><a href="/">Home</a></li>
                 <li class="breadcrumb-item"><a href="#">{{$slug1}}</a></li>
                 <li class="breadcrumb-item">{{$slug_detail->nav_name ?? ''}}</li>
               </ol>
